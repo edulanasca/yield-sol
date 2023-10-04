ae52f48d646749587bf2ddde39e6bc5d865e5788
trykie <131900979+trykie@users.noreply.github.com>
2023-10-04 12:49:20 -0600

    Update frontend

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Header from "@/components/Header";
+import {Providers} from "@/providers"
+
+require('@solana/wallet-adapter-react-ui/styles.css')
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +20,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <Providers>
+      <Header/>
+      {children}
+      </Providers>
+      </body>
     </html>
   )
 }
---
+++ frontend/src/providers.tsx
@@ -0,0 +1,25 @@
+'use client'
+import {useMemo} from "react"
+import {clusterApiUrl} from "@solana/web3.js"
+import {WalletAdapterNetwork} from "@solana/wallet-adapter-base"
+import {WalletModalProvider} from "@solana/wallet-adapter-react-ui"
+import {PhantomWalletAdapter} from "@solana/wallet-adapter-wallets"
+import {ConnectionProvider, WalletProvider} from "@solana/wallet-adapter-react"
+
+
+// @ts-ignore
+export function Providers({children}) {
+    const network = WalletAdapterNetwork.Devnet;
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+    const wallets = useMemo(() => [new PhantomWalletAdapter()], [network])
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    {children}
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    )
+}
