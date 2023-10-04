aeef7f144b2dbe8f08ba2e0b0c06f99d5b6f1fda
trykie <131900979+trykie@users.noreply.github.com>
2023-10-04 15:30:23 -0600

    Update borrow page

--- frontend/src/app/borrow/page.tsx
+++ frontend/src/app/borrow/page.tsx
@@ -1,18 +1,57 @@
 "use client";
 
-import {useState} from "react";
+import {ChangeEvent, useEffect, useState} from "react";
 import {useWallet} from "@solana/wallet-adapter-react";
+
+interface IToken {
+    name: string
+    symbol: string
+    apr: number
+}
 
 export default function Borrow() {
     const {publicKey} = useWallet()
     const [amount, setAmount] = useState('')
+    const [token, setToken] = useState<IToken>()
+    const [tokenList, setTokenList] = useState<IToken[]>([])
+
+    // TODO: Remove when loadTokenList is complete
+    const initList = [
+        {
+            id: 1,
+            name: 'Solana',
+            symbol: 'SOL',
+            apr: 5.6,
+        },
+        {
+            id: 2,
+            name: 'USDC Token',
+            symbol: 'USDC',
+            apr: 4.3,
+        }
+    ]
+
+    // TODO: Load token list
+    const loadTokenList = () => {
+        setTokenList(initList)
+    }
 
     // TODO: Implement action
-    const HandelBorrow = () => {
+    const handleBorrow = () => {
         console.log('Call contract')
         console.log(amount)
+        console.log(token)
         console.log(publicKey)
     }
+
+    const handleOnChangeToken = (e: ChangeEvent<HTMLSelectElement>) => {
+        const token = tokenList.find((t) => t.name === e.target.value)
+        setToken(token)
+    }
+
+    useEffect(() => {
+        loadTokenList()
+    }, [])
 
     return (
         <div className={'flex justify-center'}>
@@ -20,7 +59,7 @@ export default function Borrow() {
                 <div className={'text-4xl font-bold text-purple-700'}>BORROW</div>
                 <div className={'text-gray-500 text-sm my-2'}>
                     Borrow popular Solana tokens at a
-                    <span className={'font-bold'}>fixed rate</span>
+                    {' '}<span className={'font-bold'}>fixed rate</span>
                 </div>
                 <div className={'my-10'}>
 
@@ -29,12 +68,12 @@ export default function Borrow() {
                                className="block w-full rounded-full px-5 py-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400"
                                placeholder="Enter amount" value={amount} onChange={(e) => setAmount(e.target.value)}/>
                         <div className="absolute inset-y-0 right-5 flex items-center">
-                            <label htmlFor="token" className="sr-only">Token</label>
                             <select id="token" name="token"
-                                    className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
-                                <option>Token1</option>
-                                <option>Token2</option>
-                                <option>Token3</option>
+                                    className="h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                                    onChange={(e) => handleOnChangeToken(e)}
+                            >
+                                {tokenList.map((token) => <option key={token.name}
+                                                                  value={token.name}>{token.symbol}</option>)}
                             </select>
                         </div>
                     </div>
@@ -42,14 +81,14 @@ export default function Borrow() {
                 </div>
                 <div className={'my-10'}>
                     <div className={'rounded-full bg-green-200 w-fit py-4 px-6'}>
-                        <span className={'text-2xl font-bold'}>1.23</span>
+                        <span className={'text-2xl font-bold'}>{token ? token.apr : '~'}</span>
                         <span>%APR</span>
                     </div>
                 </div>
                 <div className={'mt-16'}>
                     <button
                         className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full w-full"
-                        onClick={HandelBorrow}
+                        onClick={handleBorrow}
                     >
                         Next Step
                     </button>
